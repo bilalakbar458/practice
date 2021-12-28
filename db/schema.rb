e98a6fe8f6113936a3271c1a489bfadab15f85f8
Bilal Raza <bilalakbar458@gmail.com>
2021-12-28 08:52:28 +0500

    Added portfolio items via resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_032408) do
+ActiveRecord::Schema.define(version: 2021_12_28_034550) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
